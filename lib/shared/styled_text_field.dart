30ece6897fc157bbb707339853488fd9b18365a5
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-21 15:23:12 +0800

    feat: added a icon field in he reusable widget for textfield

--- lib/shared/styled_text_field.dart
+++ lib/shared/styled_text_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class StyledTextField extends StatelessWidget {
+class StyledTextField extends StatefulWidget {
   final TextEditingController controller;
   final String label;
   final bool obscureText;
@@ -15,14 +15,39 @@ class StyledTextField extends StatelessWidget {
   });
 
   @override
+  State<StyledTextField> createState() => _StyledTextFieldState();
+}
+
+class _StyledTextFieldState extends State<StyledTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller,
-      obscureText: obscureText,
-      keyboardType: keyboardType,
-      decoration: InputDecoration(
-        labelText: label,
-      ),
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      obscureText: _isObscured,
+        decoration: InputDecoration(
+          labelText: widget.label,
+          suffixIcon: widget.obscureText ? IconButton(
+              onPressed: () {
+                setState(() {
+                  _isObscured = !_isObscured;
+                });
+              },
+              icon: Icon(
+                  _isObscured
+                      ? Icons.visibility
+                      : Icons.visibility_off
+              ),
+          ) : null,
+        )
     );
   }
 }
